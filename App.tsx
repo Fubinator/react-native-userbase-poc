e11307f9021fa72fefbb1a86639b283a9837b8bb
Fabian Kutsche <kutschefabian@gmail.com>
2021-04-25 15:08:46 +0200

    add: component to test all userbase methods

--- App.tsx
+++ App.tsx
@@ -1,37 +1,12 @@
 import React, {useEffect} from 'react';
-import userbase from 'userbase-js';
 import initialize from './userbase';
-import config from './config';
-
-import {SafeAreaView, StatusBar, Text, View} from 'react-native';
+import {SafeAreaView, StatusBar, View} from 'react-native';
+import UserbaseTestButtons from './UserbaseTestButtons';
 
 const App = () => {
   useEffect(() => {
     async function init() {
-      try {
-        await initialize();
-
-        const session = await userbase.init({
-          appId: config.USERBASE_APP_ID,
-        });
-        console.log('SESSION:', session);
-
-        if (!session.user) {
-          const user = await userbase.signIn({
-            username: config.USERBASE_USERNAME,
-            password: config.USERBASE_PASSWORD,
-            rememberMe: 'local',
-          });
-
-          console.log('USER: ', user);
-        }
-
-        const dbs = await userbase.getDatabases();
-
-        console.log('DB OPEN', dbs);
-      } catch (error) {
-        console.log(error);
-      }
+      await initialize();
     }
     init();
   }, []);
@@ -40,7 +15,7 @@ const App = () => {
     <SafeAreaView>
       <StatusBar />
       <View>
-        <Text>React Native Userbase POC</Text>
+        <UserbaseTestButtons />
       </View>
     </SafeAreaView>
   );
